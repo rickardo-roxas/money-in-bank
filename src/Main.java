1d116db0d6e192cb8047ef0484f346bc91d7bec4
Johan Rickardo A. Roxas <2233375@slu.edu.ph>
2023-06-01 11:56:53 +0800

    Added colors.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Johan Rickardo Roxas
@@ -6,6 +7,36 @@ import javax.swing.*;
  * Main program that contains the graphical user-interface.
  */
 public class Main extends JFrame {
+
+    /**
+     * Midnight green
+     * Primary color for UI
+     */
+    private final Color midnightGreen = new Color(9,82,96);
+
+    /**
+     * Teal
+     * Secondary color for UI
+     */
+    private final Color teal = new Color(8,127,140);
+
+    /**
+     * Zomp
+     * Secondary color for UI
+     */
+    private final Color zomp = new Color(90,170,149);
+
+    /**
+     * Asparagus
+     * Primary color for UI
+     */
+    private final Color asparagus = new Color(134,168,115);
+
+    /**
+     * Satin sheen gold
+     * Primary color for UI
+     */
+    private final Color satinSheenGold = new Color(187,159,6);
 
     /**
      * Main entry point of the program
@@ -27,12 +58,17 @@ public class Main extends JFrame {
     public Main() {
         super("Money In Bank");
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1200,600);
-
+        // Sets the icon image
         ImageIcon favicon = new ImageIcon("icons/favicon.png");
         setIconImage(favicon.getImage());
 
+        // Left Panel
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(Color.RED);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1200,700);
+        setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     } // end of Main constructor
