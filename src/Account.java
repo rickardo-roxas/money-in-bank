9ceaedf6665172645efd69a737cb73f087c075d0
Johan Rickardo A. Roxas <2233375@slu.edu.ph>
2023-06-01 10:15:12 +0800

    Added Account.java reference class with documentation

--- src/Account.java
+++ src/Account.java
@@ -4,8 +4,17 @@
  * Template for Account object.
  */
 public class Account {
+    /**
+     * Name of account holder
+     */
     private String accountHolder;
+    /**
+     * Account number (XXX XXX XXXX)
+     */
     private String accountNumber;
+    /**
+     * Outstanding balance of the account
+     */
     private double balance;
 
     /**
@@ -17,36 +26,70 @@ public class Account {
         balance = 10000.00;
     } // end of Account default constructor
 
+    /**
+     * Constructs an object of Account with user-defined values
+     * @param accountHolder given name of account holder
+     * @param accountNumber given account number
+     * @param balance given balance
+     */
     public Account(String accountHolder, String accountNumber, double balance) {
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
         this.balance = balance;
     } // end of Account constructor
 
+    /**
+     * Mutates the state of the accountHolder attribute
+     * @param accountHolder given value
+     */
     public void setAccountHolder(String accountHolder) {
         this.accountHolder = accountHolder;
-    }
+    } // end of setAccountHolder mutator method
 
+    /**
+     * Mutates the state of the accountNumber attribute
+     * @param accountNumber given value
+     */
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
-    }
+    } // end of setAccountNumber mutator method
 
+    /**
+     * Mutates the state of the balance attribute
+     * @param balance given value
+     */
     public void setBalance(double balance) {
         this.balance = balance;
-    }
+    } // end of setBalance mutator method
 
+    /**
+     * Accesses the state of the accountHolder attribute
+     * @return state/value of accountHolder attribute
+     */
     public String getAccountHolder() {
         return accountHolder;
-    }
+    } // end of getAccountHolder accessor method
 
+    /**
+     * Accesses the state of the accountNumber attribute
+     * @return state/value of accountNumber attribute
+     */
     public String getAccountNumber() {
         return accountNumber;
-    }
+    } // end of getAccountNumber accessor method
 
+    /**
+     * Accesses the state of the balance attribute
+     * @return state/value of balance attribute
+     */
     public double getBalance() {
         return balance;
-    }
+    } // end of getBalance accessor method
 
+    /**
+     * Concatenates the attribute states of Account
+     * @return states as comma-separated values
+     */
     public String toString() {
         return accountHolder + "," +
                 accountNumber + "," +
